1279fa5f49b142529aa9a09b28067203827b8cc1
Noman Ejaz <nomanejaz132@gmail.com>
2023-09-15 19:12:35 +0500

    problem 10 ...

--- day10/amusement-park.js
+++ day10/amusement-park.js
@@ -82,3 +82,13 @@ const tickets = {
 ticketStatus(tickets, 'RE90VAW7');
 ticketStatus(tickets, '0H2AZ123');
 ticketStatus(tickets, '23LA9T41');
+
+// Task 4: Improve the ticket status response
+
+// After a while, you get feedback from the employees that they want the ticket status to be easier to understand at the first glance. 
+// They only want to see either the name of the visitor or that the ticket is invalid.
+
+// Write a function simpleTicketStatus that accepts the same arguments as ticketStatus in task 3. This function only returns one of two different results.
+
+// the name of the visitor if the ticket was sold
+// 'invalid ticket !!!' if the ticket was not sold yet or the identifier was not found in the tracking object
